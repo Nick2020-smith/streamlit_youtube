528fd868bbfa358b64796e3aeca12fb40273253f
Seph1331 <64813736+Nick2020-smith@users.noreply.github.com>
2024-08-16 17:18:03 -0500

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,15 +1,6 @@
 from pytube import YouTube
 
-def download(link):
-    try:
-        yt = YouTube(link).streams
-        video_stream = yt.get_highest_resolution()
-        video_stream.download()
-        print("Descarga Completa", "Video descargado satisfactoriamente!")
-    except Exception as e:
-        print("Error", f"Ha ocurrido un error: {str(e)}")
-
-if __name__ == '__main__':
-    link = input("Link de Youtube AQUI:")
-    download(link)
-    
+url = "https://www.youtube.com/watch?v=Y5fBdpreJiU"
+mi_video = YouTube(url)
+mi_video = mi_video.streams.get_highest_resolution()
+mi_video.download() 
